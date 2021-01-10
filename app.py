76b257cb37e9d16084cc4411a17d7e54daa04749
Ramansh-03 <sangalramansh@gmail.com>
2021-01-10 18:15:47 +0530

    diabetes

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request
+import pickle
+import numpy as np
+file='diabetes.pkl'
+rf_random=pickle.load(open(file,'rb'))
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    if(request.method=='POST'):
+        preg = int(request.form['pregnancies'])
+        glucose = int(request.form['glucose'])
+        bp = int(request.form['bloodpressure'])
+        st = int(request.form['skinthickness'])
+        insulin = int(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        dpf = float(request.form['dpf'])
+        age = int(request.form['age'])
+        
+        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+        my_prediction = rf_random.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+if __name__=='__main__':
+    app.run(debug=True)
